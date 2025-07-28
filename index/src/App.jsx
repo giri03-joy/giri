4c39e96203618f4bf1b227e532aaa530ff08e087
giri03-joy <girivardhanreddy03@gmail.com>
2025-07-28 11:27:16 +0530

    success message

--- index/src/App.jsx
+++ index/src/App.jsx
@@ -10,11 +10,13 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+         <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={sampuImage} alt="Sampu" />
+          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+
       </div>
       <h1>sampuuuuu</h1>
       <div className="card">
